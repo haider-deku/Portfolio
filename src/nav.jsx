e66fdd483a54db4892634c77a1da0b170603347a
haider youzbechi <84813870+haider-deku@users.noreply.github.com>
2025-06-07 00:52:11 +0200

    Update nav.jsx

--- src/nav.jsx
+++ src/nav.jsx
@@ -3,7 +3,7 @@ const Nav = () => {
          <nav>
             <a href="#exp">EXPERIENCE</a>
             <a href="#edu">EDUCATION</a>
-            <a href="https://github.com/haider-deku" target="_blank">PROJECT</a>
+            <a href="https://github.com/haider-deku" target="_blank" rel="noreferrer">PROJECT</a>
             <a href="#sk">SKILLS</a>
             <a href="#cont">CONTACT</a>
         </nav>
